aa963494e75cd7318f849339603dcfd46df79506
Hack3rM <135494364+Hack3rM@users.noreply.github.com>
2023-06-04 08:41:53 +0300

    Add files via upload

---
+++ tool.py
@@ -0,0 +1,18 @@
+#!/user/bin/python3
+
+
+
+print ( input ("do you went to start this tool: "))
+name = input ("what is your name: ")
+if name == "Yared" or   name == "Miki" :
+              ask = input("are you bad person: ")
+              if ask == "yes":
+                     print("get out")
+                     exit()
+              else:
+                         print("ok sory")
+                         
+               
+else:
+               print("welocme")
+ 
